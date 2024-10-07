fca736092bb14062088bfcf4c5e84801a8fe6ea9
Kev58205 <kevinkariuki286@gmail.com>
2024-10-08 00:49:43 +0300

    script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function showLogin() {
+  document.getElementById('form-container').style.transform = 'translateX(0)';
+}
+
+function showRegister() {
+  document.getElementById('form-container').style.transform = 'translateX(-50%)';
+}
